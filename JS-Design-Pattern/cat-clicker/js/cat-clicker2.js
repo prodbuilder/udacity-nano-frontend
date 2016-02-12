2fa73fec60fe5cdeb5236733cd3d1fea425d1762
Yu Guo <yuguo01462@gmail.com>
2016-02-11 22:08:58 -0800

    [JSDP] add admin view and closure

--- JS-Design-Pattern/cat-clicker/js/cat-clicker2.js
+++ JS-Design-Pattern/cat-clicker/js/cat-clicker2.js
@@ -5,7 +5,7 @@ $(function() {
         this.id = id;
         this.count = 0;
     };
-
+    /* ======= Model ======= */
     var model = {
         init: function() {
             if (!localStorage.cats) {
@@ -13,8 +13,8 @@ $(function() {
                     new Cat('img/cat1.jpg', 'Cat 1', 0),
                     new Cat('img/cat2.jpg', 'Cat 2', 1),
                     new Cat('img/cat3.jpg', 'Cat 3', 2),
-                    new Cat('img/cat1.jpg', 'Cat 4', 3),
-                    new Cat('img/cat2.jpg', 'Cat 5', 4),
+                    new Cat('img/cat4.jpg', 'Cat 4', 3),
+                    new Cat('img/cat5.jpg', 'Cat 5', 4),
                     new Cat('img/cat3.jpg', 'Cat 6', 5),
                 ]);
             }
@@ -25,73 +25,136 @@ $(function() {
         putCats: function(cats) {
             localStorage.cats = JSON.stringify(cats);
         },
-        increaseCount: function(id) {
+        getCati: function(id) {
+            return this.getCats()[id];
+        },
+        putCati: function(id, cat) {
             var cats = this.getCats();
-            cats.forEach(function(cat) {
-                if (cat.id === id) {
-                    cat.count++;
-                }
-            })
+            cats[id] = cat;
             this.putCats(cats);
+        },
+        increaseCount: function(id) {
+            var cat = this.getCati(id);
+            cat.count++;
+            this.putCati(id, cat);
+        },
+        updateCati: function(id, name, img, count) {
+            var cat = this.getCati(id);
+            cat.name = name;
+            cat.img = img;
+            cat.count = count;
+            this.putCati(id, cat);
         }
     };
-
+    /* ======= Octopus ======= */
     var controller = {
         init: function() {
             model.init();
-            view.init();
+            // set our current cat to the first one in the list
+            this.setCurrentCat(0);
+            catView.init();
+            catListView.init();
+            adminView.init();
+
         },
         getCats: function() {
             return model.getCats();
         },
-        increaseCount: function(id) {
-            model.increaseCount(id);
+        setCurrentCat: function(id) {
+            this.id = id;
+        },
+        getCurrentCat: function() {
+            return this.getCats()[this.id];
+        },
+        clickCurrentCat: function() {
+            model.increaseCount(this.id);
+            catView.render();
+        },
+        updateCurrentCat: function(name, img, count) {
+            model.updateCati(this.id, name, img, count);
+            catView.render();
         }
     };
-
-    var view = {
+    /* ======= View ======= */
+    var catView = {
         init: function() {
+            var $cat = $('.cat-view');
+            this.cat_name = $cat.find('.name');
+            this.cat_img = $cat.find('img');
+            this.cat_cnt = $cat.find('.count');
             this.render();
-            $('.cat-name ul').children().first().click();
-            // $('#cat-clicker0').click();
+            this.cat_img.click(function() {
+                controller.clickCurrentCat();
+            });
         },
         render: function() {
-            this.renderList();
-            this.renderMain();
+            console.log('render current cat view')
+            this.cat = controller.getCurrentCat();
+            this.cat_name.text(this.cat.name);
+            this.cat_img.attr('src', this.cat.img);
+            this.cat_cnt.text(this.cat.count);
+            adminView.render();
         },
-        renderMain: function() {
-            var $cats = $('.cats');
-            controller.getCats().forEach(function(cat) {
-                $cats.append('<div class="" style="display: none;"> <h1><span class="cat-name" id="#"> </span> clicked: <span class="count"></span> Times</h1> <br><img class="img img-responsive" src="#" id="#"></img> </div>');
-                var elem = $cats.children().last();
-                elem.attr('id', 'cat-div' + cat.id);
-                elem.find('.cat-name').attr('id', 'cat' + cat.id).text(cat.name);
-                elem.find('img').attr('id', 'cat-clicker' + cat.id).attr('src', cat.img);
-                elem.find('.count').attr('id', 'counter' + cat.id).text(cat.count);
-
-                elem.find('img').click(function() {
-                    controller.increaseCount(cat.id);
-                    // this step fakes getting increased count, by directly displayin locally
-                    elem.find('#counter' + cat.id).text(++cat.count);
-                    console.log('clicked! id=', cat.id, 'DOM count=', elem.find('#counter' + cat.id).text(), 'cat count=', cat.count);
-                });
-            });
-        },
-        renderList: function() {
-            var $catList = $('.cat-name ul');
-            controller.getCats().forEach(function(cat) {
-                $catList.append('<li>' + cat.name + '</li>');
-                nameElem = $catList.children().last();
-                nameElem.click(function() {
-                    var elem = $('#cat-div' + cat.id);
-                    elem.show();
-                    elem.siblings().hide();
-                });
-            });
-        },
-
     };
-
+    var catListView = {
+        init: function() {
+            this.render();
+        },
+        render: function() {
+            var $catListElem = $('.cat-name ul');
+            var cats = controller.getCats();
+            cats.forEach(function(cat) {
+                $catListElem.append('<li>' + cat.name + '</li>');
+                $catListElem.children().last().click(function() {
+                    controller.setCurrentCat(cat.id);
+                    // TODO: how to move this to controller?
+                    catView.render();
+                    adminView.hide();
+                });
+            });
+        },
+    };
+    var adminView = {
+        init: function() {
+            this.elem = $(".admin");
+            var $edit = $("#edit"),
+                $cancel = $("input[value='cancel']"),
+                $save = $("input[value='save']");
+            adminView.render();
+            // bind click events
+            $edit.click((function(thisCopy) {
+                return (function() {
+                    thisCopy.render();
+                    thisCopy.elem.toggle();
+                });
+            })(this));
+            $cancel.click((function(thisCopy) {
+                return (function() {
+                    thisCopy.elem.hide();
+                });
+            })(this));
+            $save.click((function(thisCopy) {
+                return (function() {
+                    thisCopy.saveCat();
+                    thisCopy.elem.hide();
+                });
+            })(this));
+        },
+        render: function() {
+            var cat = controller.getCurrentCat();
+            $("input[name='name']").val(cat.name);
+            $("input[name='image-src']").val(cat.img);
+            $("input[name='clicks']").val(cat.count);
+        },
+        saveCat: function() {
+            var name = $("input[name='name']").val(),
+                img = $("input[name='image-src']").val(),
+                count = $("input[name='clicks']").val();
+            controller.updateCurrentCat(name, img, count);
+        },
+        hide: function() {
+            this.elem.hide();
+        }
+    };
     controller.init();
-    window.view = view;
 });
