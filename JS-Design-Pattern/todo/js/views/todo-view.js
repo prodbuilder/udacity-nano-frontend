64792e12d79e938c3338e3aa6250f2bea3b34008
Yu Guo <yuguo01462@gmail.com>
2016-02-12 03:22:18 -0800

    [JSDP] todo: add priority

--- JS-Design-Pattern/todo/js/views/todo-view.js
+++ JS-Design-Pattern/todo/js/views/todo-view.js
@@ -20,6 +20,7 @@ var app = app || {};
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
+	        'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+            this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -59,7 +61,12 @@ var app = app || {};
 		toggleVisible: function () {
 			this.$el.toggleClass('hidden', this.isHidden());
 		},
-
+        togglePriority: function() {
+            if (!this.model.get('completed')) {
+                this.$el.toggleClass('priority');
+                this.togglePriority();
+            }
+        },
 		isHidden: function () {
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
@@ -70,7 +77,10 @@ var app = app || {};
 		toggleCompleted: function () {
 			this.model.toggle();
 		},
-
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
+		},
 		// Switch this view into `"editing"` mode, displaying the input field.
 		edit: function () {
 			this.$el.addClass('editing');
