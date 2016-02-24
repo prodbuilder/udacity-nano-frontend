a978c8c49b764bb345f2ac23ff717ea7edb096a5
Yu Guo <yuguo01462@gmail.com>
2016-02-23 16:55:22 -0800

    [P2] edit gulp file

---
+++ P2-resume/app/gulpfile.js
@@ -0,0 +1,15 @@
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var gulp = require('gulp');
+gulp.task('serve', function() {
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    // edit the files you watch here
+    gulp
+        .watch(['js/*.js', 'css/*.css', 'images/*', '*.html', 'jasmine/spec/*.js'])
+        .on('change', reload);
+});
